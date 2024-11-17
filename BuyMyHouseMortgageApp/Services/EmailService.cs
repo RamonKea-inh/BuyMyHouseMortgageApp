e00a97d8a58474a8a0e9bd71172266bcba18928d
RamonKea-inh <687187@student.inholland.nl>
2024-11-17 22:21:50 +0100

    fixed some SendGrid problems

--- BuyMyHouseMortgageApp/Services/EmailService.cs
+++ BuyMyHouseMortgageApp/Services/EmailService.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace BuyMyHouseMortgageApp.Services
 {
@@ -17,20 +13,42 @@ namespace BuyMyHouseMortgageApp.Services
             _configuration = configuration;
         }
 
-        public Task SendOfferEmailAsync(string recipientName, string offerDetails)
+        public async Task SendOfferEmailAsync(string recipientEmail, string offerDetails)
         {
-            // Logic to send an email to the recipient. Maybe mocking the email service, because I don't have access to the email service and this is a proof of concept
-            //var apiKey = _configuration["EmailServiceApiKey"];
-            //var client = new SendGridClient(apiKey);
-            //var from = new EmailAddress(_configuration["EmailServiceSender"], "Buy My House");
-            //var subject = "Your Mortgage Offer";
-            //var to = new EmailAddress(recipientName);
-            //var plainTextContent = $"Dear {recipientName},\n\nHere are the details of your mortgage offer:\n\n{offerDetails}\n\nBest regards,\nBuy My House";
-            //var htmlContent = $"<strong>Dear {recipientName},</strong><br><br>Here are the details of your mortgage offer:<br><br>{offerDetails}<br><br>Best regards,<br>Buy My House";
-            //var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            //var response = await client.SendEmailAsync(msg);
+            try
+            {
+                var apiKey = _configuration["EmailServiceApiKey"];
+                if (string.IsNullOrEmpty(apiKey))
+                {
+                    throw new InvalidOperationException("SendGrid API key is not configured");
+                }
 
-            return Task.CompletedTask;
+                var client = new SendGridClient(apiKey);
+                var from = new EmailAddress(_configuration["EmailServiceSender"], "Buy My House");
+                var to = new EmailAddress(recipientEmail);
+                var subject = "Your Mortgage Offer";
+                var plainTextContent = $"Dear {recipientEmail},\n\nHere are the details of your mortgage offer:\n\n{offerDetails}\n\nBest regards,\nBuy My House";
+                var htmlContent = $@"
+                    <div>
+                        <p><strong>Dear {recipientEmail},</strong></p>
+                        <p>Here are the details of your mortgage offer:</p>
+                        <p>{offerDetails}</p>
+                        <p>Best regards,<br/>Buy My House</p>
+                    </div>";
+
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                var response = await client.SendEmailAsync(msg);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception($"Failed to send email. Status code: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the exception here
+                throw new Exception("Failed to send email offer", ex);
+            }
         }
     }
 }
